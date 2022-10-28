87c363e12652a6da06b7a01bf8709dbea482016a
Yurith Rubio <78481690+yurith-rubio@users.noreply.github.com>
2022-10-28 21:25:09 +0200

    Header fixes dev (#21)
    * Update from Shopify for theme Bettdrucke-2022v4.0/HeaderFixes_dev 
    
     Committed from shop: Bettdrucke.de
    
    * Update from Shopify for theme Bettdrucke-2022v4.0/HeaderFixes_dev
    
    Committed from shop: Bettdrucke.de
    
    * Update from Shopify for theme Bettdrucke-2022v4.0/HeaderFixes_dev
    
    Committed from shop: Bettdrucke.de
    
    * Update from Shopify for theme Bettdrucke-2022v4.0/HeaderFixes_dev
    
    Committed from shop: Bettdrucke.de
    
    * Update from Shopify for theme Bettdrucke-2022v4.0/HeaderFixes_dev
    
    Committed from shop: Bettdrucke.de
    
    * Update from Shopify for theme Bettdrucke-2022v4.0/HeaderFixes_dev
    
    Committed from shop: Bettdrucke.de
    
    * Update from Shopify for theme Bettdrucke-2022v4.0/HeaderFixes_dev
    
    Committed from shop: Bettdrucke.de
    
    * Update from Shopify for theme Bettdrucke-2022v4.0/HeaderFixes_dev
    
    Committed from shop: Bettdrucke.de
    
    * Update from Shopify for theme Bettdrucke-2022v4.0/HeaderFixes_dev
    
    Committed from shop: Bettdrucke.de
    
    Co-authored-by: shopify[bot] <79544226+shopify[bot]@users.noreply.github.com>

--- assets/hide-and-show-menu.js
+++ assets/hide-and-show-menu.js
@@ -1,13 +1,11 @@
 // Show menu when scrolling up and Hide Menu when scrolling down
-var prevScrollpos = window.pageYOffset;
+let prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
-  var currentScrollPos = window.pageYOffset;
+  let currentScrollPos = window.pageYOffset;
   if (currentScrollPos <= 185){
-    document.getElementById("header").style.top = "0px";
-    document.getElementById("header").style.position = "relative";
-  } else if (prevScrollpos > currentScrollPos) {
     document.getElementById("header").style.top = "0";
-    document.getElementById("header").style.position = "fixed";
+  } else if (prevScrollpos > currentScrollPos) {
+    document.getElementById("header").style.top = "-37px";
   } else if (currentScrollPos >= 220) {
     document.getElementById("header").style.top = "-231px";
   }
